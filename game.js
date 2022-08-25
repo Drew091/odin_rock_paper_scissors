e34f69084e3bf08d62abd44a3293d165cacefb34
Drew091 <hoppal.andras91@gmail.com>
2022-08-25 13:32:57 +0200

    Create playRound()

--- game.js
+++ game.js
@@ -24,11 +24,35 @@ function getComputerChoice() {
   return computerChoiseStr;
 }
 
+function getPlayerChoice() {
+
+  let playerChoiceStr = prompt("Rock Papper or Scissor?","");
+  playerChoiceStr = playerChoiceStr.charAt(0).toUpperCase() + playerChoiceStr.slice(1).toLowerCase();
+  console.log(playerChoiceStr);
+  return playerChoiceStr;
+}
+
+function playRound () {
+   const playerSelection = getPlayerChoice();
+   const computerSelection = getComputerChoice();
+
+  if (playerSelection == "Rock" && computerSelection == "Paper") {
+    console.log("You Lose! Paper beats Rock");
+  }else if (playerSelection == "Paper" && computerSelection == "Scissor") {
+    console.log("You Lose! Scissor beats Paper");
+  }else if (playerSelection == "Scissor" && computerSelection == "Rock") {
+    console.log("You Lose! Rock beats Scissor");
+  }else if (playerSelection == computerSelection) {
+    console.log("Tie!");
+  }else{
+    console.log("You Win! " + playerSelection + " beats " + computerSelection);
+  }
+}
+
 function game() {
 
-  let playerChoice = prompt("Rock Papper or Scissor?","");
-  playerChoice = playerChoice.charAt(0).toUpperCase() + playerChoice.slice(1).toLowerCase();
-  console.log(playerChoice);
-
+  getPlayerChoice();
   getComputerChoice();
+
+
 }
